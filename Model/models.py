8eab86cfc3136a8140ca8535bf38ab3b2aa25fe6
Khang <kt36@njit.edu>
2022-11-29 17:33:23 -0500

    init

--- Model/models.py
+++ Model/models.py
@@ -18,16 +18,13 @@ class NormNN(nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(NormNN, self).__init__()
         self.layer_1 = nn.Linear(input_dim, hidden_dim)
-        # nn.init.kaiming_uniform_(self.layer_1.weight, nonlinearity="relu")
-        self.layer_2 = nn.Linear(hidden_dim, hidden_dim)
-        self.layer_3 = nn.Linear(hidden_dim, output_dim)
+        nn.init.kaiming_uniform_(self.layer_1.weight, nonlinearity="relu")
+        self.layer_2 = nn.Linear(hidden_dim, output_dim)
 
     def forward(self, x):
-        x1 = torch.nn.functional.relu(self.layer_1(x))
-        # bound norm to 1
-        x2 = torch.nn.functional.relu(self.layer_2(x1))
-        norm = torch.norm(x2, dim=-1, keepdim=True).repeat(1, x2.size(dim=-1)) + 1e-16
-        x3 = torch.div(x2, norm)
-        x4 = self.layer_3(x3)
-        x5 = torch.nn.functional.sigmoid(x4)
-        return x5
+        x = torch.nn.functional.relu(self.layer_1(x))
+        norm = torch.norm(x, dim=-1, keepdim=True).repeat(1, x.size(dim=-1)) + 1e-16
+        x = torch.div(x, norm)
+        x = self.layer_2(x)
+        out = torch.nn.functional.sigmoid(x)
+        return out
