744b6e2fca7d214b24f038a41157dbc1d7e66f1c
Khang <kt36@njit.edu>
2022-12-26 11:13:01 -0500

    init

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
-conda install pandas numpy scikit-learn tqdm matplotlib scipy -numpy
+conda install pandas numpy scikit-learn tqdm matplotlib scipy -y
 conda install -c conda-forge opacus -y
 conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.3 -c pytorch -y
 mkdir results
